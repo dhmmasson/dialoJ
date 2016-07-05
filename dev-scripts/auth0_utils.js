7c9747c2ddb8f09a8a3253c6d920f3d022974c46
Dimitri H. Masson <dhm.masson+github@gmail.com>
2016-07-05 10:12:32 +0200

    repair callback?

--- dev-scripts/auth0_utils.js
+++ dev-scripts/auth0_utils.js
@@ -1,18 +1,34 @@
-  var lock = new Auth0Lock('EdNxwib3UPPqgPjZZbcAcmRN6wglUNQr', 'app52265043.auth0.com', {
-      container: 'root'
-    ,  socialBigButtons: true
-    , primaryColor : '#ffa726'
-    , gravatar : false 
-    , dict: 'fr'
-    , title : "coucou"
-    , icon: '/img/dialoJ.png'
-    , callbackURL: 'https://dialo-j.herokuapp.com/callback'
-    , responseType: 'code'
-    , theme : 
-    	{ logo: "/img/dialoJ.png"
-      , primaryColor: "#ffa726"    
-    	}
-    , authParams: {
-      scope: 'openid email'  // Learn about scopes: https://auth0.com/docs/scopes
-    }});    
-  lock.show();
+  // var lock = new Auth0Lock('EdNxwib3UPPqgPjZZbcAcmRN6wglUNQr', 'app52265043.auth0.com', {
+  //     container: 'root'
+  //   ,  socialBigButtons: true
+  //   , primaryColor : '#ffa726'
+  //   , gravatar : false 
+  //   , dict: 'fr'
+  //   , title : "coucou"
+  //   , icon: '/img/dialoJ.png'
+  //   , callbackURL: 'https://dialo-j.herokuapp.com/callback'
+  //   , responseType: 'code'
+  //   , theme : 
+  //   	{ logo: "/img/dialoJ.png"
+  //     , primaryColor: "#ffa726"    
+  //   	}
+  //   , authParams: {
+  //     scope: 'openid email'  // Learn about scopes: https://auth0.com/docs/scopes
+  //   }});    
+  // lock.show();
+
+  
+var lock = new Auth0Lock('EdNxwib3UPPqgPjZZbcAcmRN6wglUNQr', 'app52265043.auth0.com');    
+    lock.show({
+       container: 'root'
+     ,  socialBigButtons: true
+     , primaryColor : '#ffa726'
+     , gravatar : false 
+     , dict: 'fr'
+     , icon: '/img/dialoJ.png'
+     , callbackURL: 'https://dialo-j.herokuapp.com/callback'
+     , responseType: 'code'
+     , authParams: {
+      scope: 'openid email'  // Learn about scopes: https://auth0.com/docs/scopes 
+    }
+  }); 
